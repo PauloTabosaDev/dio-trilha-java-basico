f0f20115214723cfcf6ec0de5599b706508571c2
Paulo Filipe <paulotabosadev@gmail.com>
2024-05-22 00:17:09 -0300

    Comentario Adicionado

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -3,3 +3,20 @@ public class TiposVariaveis {
         System.out.println("Hello, World!");
     }
 }
+
+/*  A Expressão abaixo é um exemplo do operador ternario que atua como um IF e ELSE
+
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println("resultado");
+    }
+}
+
+
+Caso o metodo não retorne nenhum valor ele será represetado pela palavra-chave void
+*/
